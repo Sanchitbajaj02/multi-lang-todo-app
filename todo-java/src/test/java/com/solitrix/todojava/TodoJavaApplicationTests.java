ff63dc12e756a41b27fed59d96c02c7b5fdb4ed8
Sanchit Bajaj <sanchit02@outlook.com>
2025-08-04 00:46:43 +0530

    base commit setup for todo app: java

---
+++ todo-java/src/test/java/com/solitrix/todojava/TodoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solitrix.todojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
