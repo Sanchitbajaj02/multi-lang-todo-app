7dd2225aba8b3370206019a50ddadea57b2ba262
Sanchit Bajaj <sanchit02@outlook.com>
2025-09-14 18:09:20 +0530

    task controller creation

---
+++ todo-ts/src/controllers/task.controller.ts
@@ -0,0 +1,71 @@
+import { Dependencies } from "@/container/di.container";
+import TaskService from "@/services/task.service";
+import { Request, Response } from "express";
+
+export default class TaskController {
+  constructor(
+    private taskService: TaskService,
+    private deps: Dependencies
+  ) {}
+
+  getTasks = async (req: Request, res: Response) => {
+    try {
+      const tasks = await this.taskService.getAllTasks();
+      res.status(200).json(tasks);
+    } catch (error) {
+      this.deps.logger.error("Error fetching tasks:", error);
+      res.status(500).json({ error: "Failed to retrieve tasks" });
+    }
+  };
+
+  createTask = async (req: Request, res: Response) => {
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ error: "Title and description are required" });
+    }
+
+    try {
+      await this.taskService.createTask(title, description);
+      res.status(201).json({ message: "Task created successfully" });
+    } catch (error) {
+      this.deps.logger.error("Error creating task:", error);
+      res.status(500).json({ error: "Failed to create task" });
+    }
+  };
+
+  deleteTask = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ error: "Task ID is required" });
+    }
+
+    try {
+      await this.taskService.deleteTask(Number(id));
+      res.status(200).json({
+        message: "Task deleted successfully",
+      });
+    } catch (error: any) {
+      this.deps.logger.error("Error deleting task:", error);
+      res.status(500).json({ error: error.message || "Failed to delete task" });
+    }
+  };
+
+  toggleTask = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ error: "Task ID is required" });
+    }
+
+    try {
+      const currentStatus = await this.taskService.toggleTask(Number(id));
+      res.status(200).json({
+        message: `Task has been marked as ${currentStatus ? "completed" : "incomplete"}`,
+      });
+    } catch (error) {
+      this.deps.logger.error("Error toggling task:", error);
+      res.status(500).json({ error: "Failed to toggle task" });
+    }
+  };
+}
