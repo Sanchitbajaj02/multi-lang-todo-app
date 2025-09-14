5e19b647b7d06d3077e35b175330d18e7a86f17e
Sanchit Bajaj <sanchit02@outlook.com>
2025-09-14 18:08:43 +0530

    configure drizzle for commands

--- todo-ts/src/config/drizzle.config.ts
+++ todo-ts/src/config/drizzle.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "drizzle-kit";
+import config from "@/config/app.config";
 
 export default defineConfig({
-  dialect: "postgresql",
+  dialect: "mysql",
   schema: "./src/model/schema.ts",
   out: "./migrations",
+  dbCredentials: {
+    host: config.database.connectionHost,
+    user: config.database.connectionUser,
+    password: config.database.connectionPassword,
+    database: config.database.connectionDatabase,
+  }
 });
