46c86d751185ec8587495b49a7048cb2c94db221
Sanchit Bajaj <sanchit02@outlook.com>
2025-09-14 18:09:12 +0530

    task service creation

---
+++ todo-ts/src/services/task.service.ts
@@ -0,0 +1,56 @@
+import { Dependencies } from "@/container/di.container";
+import CustomError from "@/lib/custom-error";
+import { taskSchema } from "@/model/schema";
+import { StatusCodes } from "http-status-codes";
+
+export default class TaskService {
+  private dbClient: any;
+  constructor(private deps: Dependencies) {}
+
+  getAllTasks = async () => {
+    this.dbClient = await this.deps.databaseService.getConnection();
+
+    const tasks = await this.dbClient.select().from(taskSchema);
+    return tasks;
+  };
+
+  createTask = async (title: string, description: string) => {
+    this.dbClient = await this.deps.databaseService.getConnection();
+
+    await this.dbClient
+      .insert(taskSchema)
+      .values({ title, description })
+      .execute();
+  };
+
+  deleteTask = async (id: number) => {
+    this.dbClient = await this.deps.databaseService.getConnection();
+
+    // check if task exists
+    const task = await this.dbClient.select().from(taskSchema).where({ id });
+
+    if (task.length === 0) {
+      throw new CustomError(StatusCodes.NOT_FOUND, "Task not found");
+    }
+
+    await this.dbClient.delete(taskSchema).where({ id }).execute();
+  };
+
+  toggleTask = async (id: number) => {
+    this.dbClient = await this.deps.databaseService.getConnection();
+
+    const task = await this.dbClient.select().from(taskSchema).where({ id });
+
+    if (task.length === 0) {
+      throw new CustomError(StatusCodes.NOT_FOUND, "Task not found");
+    }
+
+    await this.dbClient
+      .update(taskSchema)
+      .set({ completed: !task[0].completed })
+      .where({ id })
+      .execute();
+
+    return !task[0].completed;
+  };
+}
