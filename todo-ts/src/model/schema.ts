2299b3d33345f06b39f96fca303a76ec906a40c2
Sanchit Bajaj <sanchit02@outlook.com>
2025-09-14 18:09:03 +0530

    tasks model creation

--- todo-ts/src/model/schema.ts
+++ todo-ts/src/model/schema.ts
@@ -1,15 +1,12 @@
 import { mysqlTable as table } from "drizzle-orm/mysql-core";
 import * as t from "drizzle-orm/mysql-core";
 
-export const todos = table(
-  "todos",
+export const taskSchema = table(
+  "tasks",
   {
-    id: t.int().primaryKey().autoincrement(),
-    title: t.varchar("title", { length: 100 }),
-    description: t.text("description"),
+    id: t.int("id").primaryKey().autoincrement(),
+    title: t.varchar("title", { length: 100 }).notNull(),
+    description: t.varchar("description", { length: 256 }),
     completed: t.boolean("completed").default(false),
-  },
-  (table) => [
-    t.uniqueIndex("id_idx").on(table.id)
-  ]
+  }
 );
